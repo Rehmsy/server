9aed21badd336a29d72659ae567273d90bdb1941
carriecarter <carrie@carrie-carter.com>
2018-06-18 15:51:36 -0700

    scaffold and setup plus db created

--- db-client.js
+++ db-client.js
@@ -9,11 +9,11 @@ const Client = pg.Client;
 
 const client = new Client(DATABASE_URL);
 client.connect()
-    .then(() => console.log('connected to db', DATABASE_URL))
-    .catch(err => console.error('connection error', err));
+  .then(() => console.log('connected to db', DATABASE_URL))
+  .catch(err => console.error('connection error', err));
 
 client.on('error', err => {
-    console.error('\n**** DATABASE ERROR ****\n\n', err);
+  console.error('\n**** DATABASE ERROR ****\n\n', err);
 });
 
 module.exports = client;
