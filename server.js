ac84921e6e10e5fa9883c2d901d4dcae7ec701d2
KLPainter <KLPainter@gmail.com>
2018-06-19 12:28:23 -0700

    working route events for user

--- server.js
+++ server.js
@@ -16,5 +16,27 @@ app.use(express.static('public'));
 // connect to the database
 const client = require('./db-client');
 
+// ROUTE:  Get the events for a user
+app.get('/api/events/:id', (req, res, next) => {
+  client.query(`
+    SELECT events.id, 
+        events.user_id as "userId", 
+        events.name, 
+        events.event_date as "eventDate", 
+        events.description, 
+    COUNT(contacts.id) as count
+    FROM events
+    JOIN contacts on events.id = contacts.event_id
+    WHERE events.user_id = $1
+    GROUP BY events.id
+    ORDER BY events.event_date
+    `,
+  [req.params.id]
+  ).then(result => {
+    res.send(result.rows);
+  })
+    .catch(next);
+});
+
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('server running on port', PORT));
